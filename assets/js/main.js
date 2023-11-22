47aab14d22b899f422c8fdb9bf371999840eb9dd
Asthito <madeasthito.yogi@gmail.com>
2023-11-23 02:14:00 +0800

    Adding error state to input and fix the input to btn gap

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,19 +3,21 @@ function validateEmail(email) {
 }
 
 function subscribe() {
-	const email = document.getElementById("email").value;
+	const emailEl = document.getElementById("email");
 	const errorStatus = document.getElementById("error");
 	const successStatus = document.getElementById("success");
 	const hideContent = document.getElementById("content-input");
 
-	if (validateEmail(email)) {
+	if (validateEmail(emailEl.value)) {
 		hideContent.classList.add("hidden");
 		errorStatus.classList.add("hidden");
+        emailEl.classList.remove("input-danger");
 		successStatus.classList.remove("hidden");
 		console.log("berhasil");
 	} else {
 		hideContent.classList.remove("hidden");
 		errorStatus.classList.remove("hidden");
+        emailEl.classList.add("input-danger");
 		successStatus.classList.add("hidden");
 		console.log("gagal");
 	}
